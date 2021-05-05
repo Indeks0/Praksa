a1aad594e8af55a55f180f61bca2b82f8ef508c3
DESKTOP-QHPCIKJ\Ivan <ivanparadzik4@gmail.com>
2021-05-05 23:12:28 +0200

    Rješen projekt.

---
+++ 3. dan/WebAPI/CRUD/Controllers/CityController.cs
@@ -0,0 +1,92 @@
+﻿using CRUD.DataClasses;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CRUD.Controllers
+{
+    public class CityController : ApiController
+    {
+        public string ConnectionString = ConfigurationManager.ConnectionStrings["myConnection"].ConnectionString;
+
+        [HttpPost]
+        public HttpResponseMessage InsertData(int zipCode, [FromBody] string name)
+        {
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = this.ConnectionString;
+            connection.Open();
+
+            SqlCommand command = new SqlCommand(
+                "INSERT INTO Cities (Zip, CityName) VALUES(@zipCode, @cityName)");
+            command.Parameters.AddWithValue("@zipCode", zipCode);
+            command.Parameters.AddWithValue("@cityName", name);
+            command.Connection = connection;
+            command.ExecuteNonQuery();
+            connection.Close();
+            return Request.CreateResponse(HttpStatusCode.OK, $"Added the city with zipCode {zipCode} to the DB.");
+        }
+
+        [HttpGet]
+        public HttpResponseMessage GetData(int zipCode)
+        {
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = this.ConnectionString;
+            connection.Open();
+
+            SqlCommand command = new SqlCommand(
+                "SELECT * FROM Cities WHERE Zip = @zipCode");
+            command.Parameters.AddWithValue("@zipCode", zipCode);
+            command.Connection = connection;
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            if (reader.Read())
+            {
+                City city = new City(reader.GetInt32(0), reader.GetString(1));
+                connection.Close();
+                return Request.CreateResponse(HttpStatusCode.OK, city);
+            }
+            connection.Close();
+            return Request.CreateResponse(HttpStatusCode.BadRequest, $"No city with zipCode {zipCode} found in DB.");
+
+        }
+
+        [HttpPut]
+        public HttpResponseMessage UpdateData(int zipCode, [FromBody] string name)
+        {
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = this.ConnectionString;
+            connection.Open();
+
+            SqlCommand command = new SqlCommand(
+                "UPDATE Cities SET CityName = @cityName WHERE Zip=" + zipCode + "");
+            command.Parameters.AddWithValue("@cityName", name);
+            command.Connection = connection;
+            command.ExecuteNonQuery();
+
+            connection.Close();
+            return Request.CreateResponse(HttpStatusCode.OK, $"Updated the city with zipCode {zipCode}");
+        }
+
+        [HttpDelete]
+        public HttpResponseMessage DeleteData(int zipCode)
+        {
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = this.ConnectionString;
+            connection.Open();
+
+            SqlCommand command = new SqlCommand(
+                "DELETE FROM Cities WHERE Zip=" + zipCode + "");
+            command.Connection = connection;
+            command.ExecuteNonQuery();
+            connection.Close();
+
+            return Request.CreateResponse(HttpStatusCode.OK, $"Deleted city from DB with zipCode {zipCode}");
+        }
+    }
+}
---
+++ 3. dan/WebAPI/CRUD/Controllers/PlayersController.cs
@@ -0,0 +1,123 @@
+﻿using CRUD.DataClasses;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CRUD.Controllers
+{
+    public class PlayersController : ApiController
+    {
+        public string ConnectionString = ConfigurationManager.ConnectionStrings["myConnection"].ConnectionString;
+
+        [HttpPost]
+        public HttpResponseMessage AddToDB([FromBody] Player player)
+        {
+            SqlConnection connection = new SqlConnection();
+            List<int> zipCodes = new List<int>();
+
+            connection.ConnectionString = this.ConnectionString;
+            connection.Open();
+
+            SqlCommand commandCheckZip = new SqlCommand("SELECT Zip FROM Cities");
+            commandCheckZip.Connection = connection;
+            SqlDataReader readerCheckZip = commandCheckZip.ExecuteReader();
+
+            while (readerCheckZip.Read())
+            {
+                zipCodes.Add(readerCheckZip.GetInt32(0));
+            }
+
+            foreach (int zip in zipCodes) //provjera postoji li ZipCode u DB i hoće li se odobriti unos igrača u DB
+            {
+                if (zip == player.PlaceOfResidence.ZipCode)
+                {
+                    readerCheckZip.Close();
+                    SqlCommand commandAddToDb = new SqlCommand(
+                        "INSERT INTO Players (Id, Name, Surname, City) VALUES (@id, @name, @surname, @city);");
+                    commandAddToDb.Parameters.AddWithValue("@id", player.Id);
+                    commandAddToDb.Parameters.AddWithValue("@name", player.Name);
+                    commandAddToDb.Parameters.AddWithValue("@surname", player.Surname);
+                    commandAddToDb.Parameters.AddWithValue("@city", player.PlaceOfResidence.ZipCode);
+                    commandAddToDb.Connection = connection;
+                    commandAddToDb.ExecuteNonQuery();
+                    connection.Close();
+                    return Request.CreateResponse(HttpStatusCode.OK, "Added the player to DB.");
+                }
+            }
+            connection.Close();
+            return Request.CreateResponse(HttpStatusCode.BadRequest, $"Players PlaceOfResidence is not in DB, please add it to DB and after that add the player.");
+        }
+
+        [HttpGet]
+        public HttpResponseMessage GetAllData()
+        {
+            List<Player> team = new List<Player>();
+            SqlConnection connection = new SqlConnection();
+
+            connection.ConnectionString = this.ConnectionString;
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("SELECT Players.Id, Players.Name, Players.Surname, Players.City, Cities.CityName FROM Players, Cities WHERE Players.City = Cities.Zip ");
+            command.Connection = connection;
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    team.Add(new Player(reader.GetInt32(0), reader.GetString(1), reader.GetString(2), new City(reader.GetInt32(3), reader.GetString(4))));
+                }
+                connection.Close();
+                return Request.CreateResponse(HttpStatusCode.OK, team);
+            }
+            else
+            {
+                connection.Close();
+                return Request.CreateResponse(HttpStatusCode.BadRequest, $"Players DB is empty");
+            }
+        }
+
+        [HttpPut]
+        public HttpResponseMessage UpdatePlayer([FromBody] Player player, int id)
+        {
+            if (id != player.Id)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "Can't change the Id of a player.");
+            }
+
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = this.ConnectionString;
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("UPDATE Players SET Name = @name, Surname = @surname, City = @city WHERE Id =" + id + "");
+            command.Parameters.AddWithValue("@name", player.Name);
+            command.Parameters.AddWithValue("@surname", player.Surname);
+            command.Parameters.AddWithValue("@city", player.PlaceOfResidence.ZipCode);
+            command.Connection = connection;
+            command.ExecuteNonQuery();
+            connection.Close();
+
+            return Request.CreateResponse(HttpStatusCode.OK, "Updated the player in DB.");
+        }
+
+        public HttpResponseMessage Delete(int id)
+        {
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = this.ConnectionString;
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("DELETE FROM Players WHERE Id =" + id + "");
+            command.Connection = connection;
+            command.ExecuteNonQuery();
+
+            return Request.CreateResponse(HttpStatusCode.OK, "Deleted the player from DB."); //nisam radio provjeru
+        }
+    }
+}
+
